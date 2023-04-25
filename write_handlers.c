3bc6bbe06429ff8af447a7bbffbf3b8e7b001e21
HASSANBOUDRRA <hassan.hbmama@gmail.com>
2023-04-25 07:32:24 -0700

    work 's team by hassan and orji

--- write_handlers.c
+++ write_handlers.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
-/************************** WRITE HANDLE ***********************/
+/************************* WRITE HANDLE *************************/
 /**
- * handle_write_char - prints a string
- * by HASSAN BOUDRAA and OULIBUBE ORJI
+ * handle_write_char - Prints a string
  * @c: char types.
  * @buffer: Buffer array to handle print
- * @flags: Calculates active flags.
+ * @flags:  Calculates active flags.
  * @width: get width.
  * @precision: precision specifier
  * @size: Size specifier
@@ -14,7 +13,7 @@
  * Return: Number of chars printed.
  */
 int handle_write_char(char c, char buffer[],
-		int flags, int width, int precision, int size)
+	int flags, int width, int precision, int size)
 { /* char is stored at left and paddind at buffer's right */
 	int i = 0;
 	char padd = ' ';
@@ -32,27 +31,26 @@ int handle_write_char(char c, char buffer[],
 	{
 		buffer[BUFF_SIZE - 1] = '\0';
 		for (i = 0; i < width - 1; i++)
-			buffer[BUFF_SIZE -  i - 2] = padd;
+			buffer[BUFF_SIZE - i - 2] = padd;
 
 		if (flags & F_MINUS)
 			return (write(1, &buffer[0], 1) +
-					write(1, &buffer[BUFF_SIZE - 1], width - 1));
+					write(1, &buffer[BUFF_SIZE - i - 1], width - 1));
 		else
 			return (write(1, &buffer[BUFF_SIZE - i - 1], width - 1) +
 					write(1, &buffer[0], 1));
 	}
+
 	return (write(1, &buffer[0], 1));
 }
 
-}
-
-/******************** WRITE NUMBER ***************************/
+/************************* WRITE NUMBER *************************/
 /**
- * write_number - prints a string
+ * write_number - Prints a string
  * @is_negative: Lista of arguments
  * @ind: char types.
  * @buffer: Buffer array to handle print
- * @flags: Calculates active flags
+ * @flags:  Calculates active flags
  * @width: get width.
  * @precision: precision specifier
  * @size: Size specifier
@@ -60,7 +58,7 @@ int handle_write_char(char c, char buffer[],
  * Return: Number of chars printed.
  */
 int write_number(int is_negative, int ind, char buffer[],
-		int flags, int width, int precision, int size)
+	int flags, int width, int precision, int size)
 {
 	int length = BUFF_SIZE - ind - 1;
 	char padd = ' ', extra_ch = 0;
